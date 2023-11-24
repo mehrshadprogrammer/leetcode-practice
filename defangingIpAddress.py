ae2c8eb8e3277339652542e55dd6f223098de65d
mehrshad <141862224+mehrshadprogrammer@users.noreply.github.com>
2023-11-24 17:13:34 -0500

    Update defangingIpAddress.py
    update code defanging ip address

--- defangingIpAddress.py
+++ defangingIpAddress.py
@@ -1,12 +1,4 @@
-# ip = '1.1.1.1'
-# newIp = ip.replace('.', '[.]')
-# print(newIp)
-# class Solution:
-#     def debangingIPadd(addr: str) -> str:
-#         newAddr = addr.replace('.', '[.]')
-#         return newAddr
-# str =debangingIPadd("192.168.11.1")
-# print(str)
+
 
 class Solution:
     def debangingIPadd(self, addr: str) -> str:
